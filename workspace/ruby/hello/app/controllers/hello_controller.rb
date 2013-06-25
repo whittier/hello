1db6c91b87d3eafe594478d1670a606a99348f83
whittier <troy.whittier@octanner.com>
2013-06-25 10:49:34 -0600

    remove old files

--- workspace/ruby/hello/app/controllers/hello_controller.rb
+++
@@ -1,9 +0,0 @@
-class HelloController < ApplicationController
-  def index
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @orders }
-    end
-  end
-
-end
--- workspace/ruby/hello/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Hello::Application.config.secret_token = 'ba670ae1160f5a6f758daa696466d57430b034aa568103a6918feed8af42b493b6d8b9ebddb3ada422884ee8a10c500632201ab7dec55dde993dc473796a5127'
